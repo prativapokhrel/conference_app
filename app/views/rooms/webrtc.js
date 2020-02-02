96471f94d4dd6945b46ff303b3185e85d8f85cde
prativa_pokhrel <prativa.pokhrel95@gmail.com>
2020-02-02 10:27:47 +0545

    fix js

--- app/views/rooms/webrtc.js
+++ app/views/rooms/webrtc.js
@@ -1,13 +1,4 @@
-//= require action_cable
-//= require_self
-//= require_tree ./channels
 
-(function() {
-  this.App || (this.App = {});
-
-  App.cable = ActionCable.createConsumer();
-
-}).call(this);
 
 document.getElementById("copy-button").addEventListener("click", function(e) {
   e.preventDefault();
